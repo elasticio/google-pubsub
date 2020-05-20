c7da828cdf1f603bd3a5ee2474ce43dbb7fc5ab5
Kirill <kyrylo.levytskyi@elastic.io>
2020-05-20 13:42:42 +0300

    fix pull and verifyCredentials

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,10 +1,9 @@
 /* eslint-disable camelcase */
 const { PubSub } = require('@google-cloud/pubsub');
-const logger = require('@elastic.io/component-logger')();
 
 module.exports = async function verifyCredentials(cfg) {
-  logger.info('Start verifying credentials');
-  logger.trace('Current credentials: %j', cfg);
+  this.logger.info('Start verifying credentials');
+  this.logger.debug('Current credentials: %j', cfg);
   const { projectId, client_email, private_key } = cfg;
   let topics;
   let result = { verified: true };
@@ -21,13 +20,13 @@ module.exports = async function verifyCredentials(cfg) {
   async function listAllTopics() {
     // Lists all topics in the current project
     [topics] = await pubSubClient.getTopics();
-    logger.trace('Topics:');
-    topics.forEach(topic => logger.trace(topic.name));
+    this.logger.debug('Topics:');
+    topics.forEach(topic => this.logger.debug(topic.name));
     return topics;
   }
 
   await listAllTopics().catch((err) => {
-    logger.error(err);
+    this.logger.error(err);
     result = { verified: false };
   });
 
