3600e394501afc7a5fae850036d8fe4881378c0d
Kirill <kyrylo.levytskyi@elastic.io>
2020-05-21 09:39:40 +0300

    minor fix verifyCredentials

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -2,8 +2,9 @@
 const { PubSub } = require('@google-cloud/pubsub');
 
 module.exports = async function verifyCredentials(cfg) {
-  this.logger.info('Start verifying credentials');
-  this.logger.debug('Current credentials: %j', cfg);
+  const self = this;
+  self.logger.info('Start verifying credentials');
+  self.logger.debug('Current credentials: %j', cfg);
   const { projectId, client_email, private_key } = cfg;
   let topics;
   let result = { verified: true };
@@ -20,13 +21,13 @@ module.exports = async function verifyCredentials(cfg) {
   async function listAllTopics() {
     // Lists all topics in the current project
     [topics] = await pubSubClient.getTopics();
-    this.logger.debug('Topics:');
-    topics.forEach(topic => this.logger.debug(topic.name));
+    self.logger.debug('Topics:');
+    topics.forEach(topic => self.logger.debug(topic.name));
     return topics;
   }
 
   await listAllTopics().catch((err) => {
-    this.logger.error(err);
+    self.logger.error(err);
     result = { verified: false };
   });
 
